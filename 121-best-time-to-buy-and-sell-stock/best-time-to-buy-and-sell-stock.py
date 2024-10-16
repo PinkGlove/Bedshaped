74d93a53b720b9656667d81fcc4b6dda9b54f217
Bedshaped <47860120+PinkGlove@users.noreply.github.com>
2024-10-16 11:33:47 -0500

    Time: 737 ms (60.63%) | Memory: 27.8 MB (6.65%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        lowest = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] > lowest:
+                res = max(res, prices[i] - lowest)
+            else:
+                lowest = min(lowest, prices[i])
+        return res
